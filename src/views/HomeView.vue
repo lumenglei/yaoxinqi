add5433f290650e1501923caef47ac2f55f153bf
yxq7777 <2085894313@qq.com>
2024-05-14 19:31:39 +0800

    feat: 首页布局

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <dv-border-box-8>
+      <dv-decoration-5 style="width: 100%; height: 130px" class="decoration"></dv-decoration-5>
+    </dv-border-box-8>
+  </div>
 </template>
 
 <script>
@@ -7,3 +11,18 @@
 
 export default {}
 </script>
+<style lang="scss" scoped>
+.home {
+  width: 100vw;
+  height: 100vh;
+  background-image: url(../assets/百香果数字运营中心/bg\(1\).png);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: 0 -19px;
+  .a {
+    background-image: url(../assets/百香果数字运营中心/标题.png);
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+}
+</style>
