5e82f344b155c1ef6dbf71d951645a17448f0ad6
yxq7777 <2085894313@qq.com>
2024-05-14 19:53:27 +0800

    feat: 首页布局

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,9 @@
 <template>
-  <div class="home">
-    <dv-border-box-8>
-      <dv-decoration-5 style="width: 100%; height: 130px" class="decoration"></dv-decoration-5>
-    </dv-border-box-8>
-  </div>
+  <dv-full-screen-container class="home">
+    <div class="decoration"><img src="../assets/百香果数字运营中心/标题.png" alt="" /></div>
+  </dv-full-screen-container>
 </template>
-
 <script>
-// @ is an alias to /src
-
 export default {}
 </script>
 <style lang="scss" scoped>
@@ -16,13 +11,14 @@ export default {}
   width: 100vw;
   height: 100vh;
   background-image: url(../assets/百香果数字运营中心/bg\(1\).png);
-  background-size: cover;
+  background-size: 100% 100%;
   background-repeat: no-repeat;
   background-position: 0 -19px;
   .decoration {
-    background-image: url(../assets/百香果数字运营中心/标题.png);
-    background-size: cover;
-    background-repeat: no-repeat;
+    width: 100%;
+    img {
+      width: 100%;
+    }
   }
 }
 </style>
